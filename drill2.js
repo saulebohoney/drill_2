d240ed0105b3cf772fa85d936cf5e431c3ca7374
htwright <harrisontwright@gmail.com>
2017-04-03 13:56:56 -0500

    To Infinity

--- drill2.js
+++ drill2.js
@@ -1,6 +1,18 @@
 function jediName(firstName, lastName)	 {
-	return (lastName.slice (3) + firstName.slice(2));
+	return (lastName.slice (0,3) + firstName.slice(0,2));
 	}
 	console.log(jediName('Saule', 'Bohoney'));
 
+function beyond(num){
+	if (isFinite(num) === false){
+		console.log("And beyond.");
+	} else if (isFinite(num) === true && num > 0){
+		console.log("To infinity.");
+	} else if (isFinite(num) === true && num < 0){
+		console.log("To negative infinity.");
+	} else if (num==0){
+		console.log("Staying home.");
+	}
+}
 
+beyond(1.5);
